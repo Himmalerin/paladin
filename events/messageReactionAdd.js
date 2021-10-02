9cc721fa98229eb258662ac6496a9a733adc8373
Himmalerin <63083269+Himmalerin@users.noreply.github.com>
2021-10-02 02:54:30 -0500

    add support for scrapbooking video posts (#1)
    * add support for scrapbooking video posts

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -16,6 +16,9 @@ module.exports = {
 			}
 		}
 
+		const imageAttachment = reaction.message.attachments.find((a) => a.contentType.startsWith("image/"));
+		const videoAttachment = reaction.message.attachments.find((a) => a.contentType.startsWith("video/"));
+
 		if (reaction.count === channels.scrapbook.minimumReactions) {
 			client.channels.cache
 				.get(channels.scrapbook.id)
@@ -29,10 +32,13 @@ module.exports = {
 								icon_url: reaction.message.author.displayAvatarURL(),
 							},
 							description: reaction.message.content.length < 2000 ? reaction.message.content : "Message too long to display!",
-							...(reaction.message.attachments.first() && { image: { url: reaction.message.attachments.first().url } }),
+							...(imageAttachment && { image: { url: imageAttachment.url } }),
 							timestamp: new Date(),
 						},
 					],
+					// Videos can't be placed inside embeds like images can so attach them as regular files instead
+					// Has the unfortunate effect of making the video appear above the embed, but whatever
+					files: [...(videoAttachment ? videoAttachment.url : [])],
 				})
 				.catch(console.error);
 		}
